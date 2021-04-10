a984fc2a8867c7a20a905c210fd2f821140b8504
Khaja Minhajuddin <minhajuddin.k@gmail.com>
2021-04-09 20:41:22 -0400

    Fix bash aliases

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ repo_dir="$(dirname "$(pwd)/$0")"
 
 ln -sf "${repo_dir}/inputrc" ~/.inputrc
 ln -sf "${repo_dir}/tmux.conf" ~/.tmux.conf
-echo "${repo_dir}/bash_aliases.sh" >> ~/.bashrc
+echo "source ${repo_dir}/bash_aliases.sh" >> ~/.bashrc
