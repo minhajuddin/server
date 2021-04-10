f295939f1cf159d4b2968bbd0f8f912a00a01991
Khaja Minhajuddin <minhajuddin.k@gmail.com>
2021-04-09 20:33:13 -0400

    Force

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 
 repo_dir="$(dirname "$0")"
 
-ln -s "${repo_dir}/inputrc" ~/.inputrc
-ln -s "${repo_dir}/tmux.conf" ~/.tmux.conf
+ln -sf "${repo_dir}/inputrc" ~/.inputrc
+ln -sf "${repo_dir}/tmux.conf" ~/.tmux.conf
 echo "${repo_dir}/bash_aliases.sh" >> ~/.bashrc
